3ba09832ab59021c065d9ed345fd4cc1b4244afd
Brian aguiar <briantbini@outlook.com>
2021-04-30 09:50:17 -0300

    Pedidos sem comentarios no codigo

---
+++ Order/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pedido.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PedingPayment = 0,
+        Processing = 1 ,
+        Shipeed = 2 ,
+        Delivered = 3
+
+    }
+}
---
+++ Order/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Pedido.Entities;
+using Pedido.Entities.Enums;
+namespace Pedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1000,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PedingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+
+
+        }
+    }
+}
