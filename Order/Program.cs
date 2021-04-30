e09c5be611f1baf1b26721dfa497bf5989b57d33
Brian aguiar <briantbini@outlook.com>
2021-04-30 09:58:50 -0300

    Pedidos com comentarios de estudo

--- Order/Program.cs
+++ Order/Program.cs
@@ -15,9 +15,12 @@ namespace Pedido
             };
             Console.WriteLine(order);
 
+            // Convertendo enum para string
             string txt = OrderStatus.PedingPayment.ToString();
 
+            // Convertendo string para enum tem que ser o mesmo nome do enum
             OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
             Console.WriteLine(os);
             Console.WriteLine(txt);
 
